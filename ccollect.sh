ea4afb014f2698ba957ddd3470f09552412e7dc2
Nico Schottelius <nico@srsyg01.(none)>
2005-12-06 17:08:38 +0100

    Requirements added

--- ccollect.sh
+++ ccollect.sh
@@ -40,7 +40,7 @@ errecho()
 #
 usage()
 {
-   echo "$(basename $0): [args] <sources to backup>"
+   echo "$(basename $0): [args] <intervall name> <sources to backup>"
    echo ""
    echo "   Nico Schottelius (nico-linux-ccollect schottelius.org) - 2005-12-06"
    echo ""
@@ -135,6 +135,12 @@ while [ "$i" -lt "$no_shares" ]; do
    c_dest="$backup/destination"
    c_exclude="$backup/exclude"
 
+   #
+   # standard rsync options
+   #
+   VERBOSE=""
+   EXCLUDE=""
+
    i=$((i+1))
 
    echo "Beginning to backup \"$name\" ..."
@@ -166,7 +172,27 @@ while [ "$i" -lt "$no_shares" ]; do
    if [ -f "$c_exclude" ]; then
       errecho "Destination $c_dest does not link to a directory. Skipping."
       continue
+   
+      while read tmp; do
+         EXCLUDE="$EXCLUDE --exclude \"$tmp\""
+     done < "$c_exclude"
    fi
+
+   #
+   # clone the old directory with hardlinks
+   #
+
+   #
+   # the rsync part
+   #
+   echo rsync --delete $VERBOSE $EXCLUDE
 done
+
+#
+# Be a good parent and wait for our children, if they are running wild parallel
+#
+if [ "$PARALLEL" = 1 ]; then
+   wait
+fi
 
 rm -f "$TMP"
