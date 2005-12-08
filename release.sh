3bb1987dcbaef8bf542ac44c609af43fe56df141
Nico Schottelius <nico@srsyg01.(none)>
2005-12-08 14:46:18 +0100

    Made release script more portabel, added some more space saving statistic

--- release.sh
+++ release.sh
@@ -1,6 +1,15 @@
 NAME=$1
+TARNAME=${NAME}.tar.bz2
 
-tar cvfj ${NAME}.tar.bz2  \
+DHOST=nico@creme.schottelius.org
+DDIR=www/org/schottelius/linux/ccollect/
+DESTINATION="$DHOST:$DDIR"
+
+tar cvfj "$TARNAME"
    --exclude=.git \
    --exclude="conf/sources/*/destination/*" \
    "$NAME"
+
+scp "${TARNAME}" "$DESTINATION"
+
+ssh "$DHOST" "( cd $DDIR; tar xfj \"$TARNAME\" )"
