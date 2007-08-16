39c4e83fbc1b274ecef81d66abed12ac9437f831
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-08-16 22:27:01 +0200

    Add master script to update configuration

---
+++ tools/config-pre-0.6-to-0.6.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Nico Schottelius, 20070816-2225
+# Transfer configuration to 0.6 layout
+# Copying: GPLv3
+#
+
+if [ $# -ne 1 ]; then
+   echo "$0: ccollect-configuration directory"
+   echo ""
+   echo "   Fix pre 0.6 configuration directories to match 0.6 style"
+   echo ""
+   exit 23
+fi
+
+dir="$1"
+script=$(echo $0 | sed 's/\.sh$/.sub.sh/')
+
+find "${dir}/sources/" -name rsync_options -exec "${skript}" {} \;
