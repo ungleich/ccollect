52095b7cf0cdc0df303c0fe0dd6fdb788a5fc156
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-08-16 23:39:18 +0200

    Update e-mail

--- tools/list_ccollect_intervals.sh
+++ tools/list_ccollect_intervals.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Nico Schottelius <nico-linux //@// schottelius.org>
+# Nico Schottelius <nico-ccollect //@// schottelius.org>
 # Date: 24-Jun-2006
 # Last Modified: -
 #
@@ -8,14 +8,22 @@
 CCOLLECT_CONF=${CCOLLECT_CONF:-/etc/ccollect}
 CCOLLECT_INTERVALS="$CCOLLECT_CONF/defaults/intervals"
 
-if [ ! -d "$CCOLLECT_INTERVALS" ]; then
-   echo "No intervals defined in $CCOLLECT_INTERVALS"
-   exit 23
+me="$(basename $0)"
+
+_echo()
+{
+   echo "$me> $@"
+}
+
+if [ ! -d "${CCOLLECT_INTERVALS}" ]; then
+   _echo "No intervals defined in ${CCOLLECT_INTERVALS}"
+   exit 1
 fi
 
-cd "$CCOLLECT_INTERVALS"
+set -e
+cd "${CCOLLECT_INTERVALS}"
 
 for interval in *; do
-   eval int_$interval=$(cat $interval);
-   eval echo $interval: \$int_$interval;
+   eval int_${interval}=$(cat "${interval}");
+   eval echo ${interval}: \$int_${interval};
 done
