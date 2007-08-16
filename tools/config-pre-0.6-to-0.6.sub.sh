967613b4c54d05dfd9fcfb6df3b1be49f64fcd15
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-08-16 22:30:02 +0200

    Add subscript

---
+++ tools/config-pre-0.6-to-0.6.sub.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Nico Schottelius, 20070816-2227
+# Transfer configuration to 0.6 layout (subscript)
+# Copying: GPLv3
+#
+
+if [ $# -ne 1 ]; then
+   echo "$0: rsync_options file"
+   echo ""
+   echo "   Fix pre 0.6 configuration directories to match 0.6 style (sub)"
+   echo ""
+   exit 23
+fi
+
+tmp=$(mktemp)
+
+for option in $(cat "$1"); do
+   echo "${option}" >> "${tmp}"
+done
+mv ${tmp} "$1"
