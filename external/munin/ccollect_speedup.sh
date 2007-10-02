6c2b41ee10fb97a979f63fbcfedc5b20e2b2ff3f
Nico Schottelius <nico@denkbrett.schottelius.org>
2007-10-02 14:18:26 +0200

    Add first munin plugin base

---
+++ external/munin/ccollect_speedup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# Nico Schottelius
+# 2007-10-02
+# speedup
+
+CCOLLECT_CONF=${CCOLLECT_CONF:-/etc/ccollect}
+CSOURCES=${CCOLLECT_CONF}/sources
+CCOLLECT_LOGDIR=${CCOLLECT_LOGDIR:-/var/log/backup}
+LOGFILE=$(cd "${CCOLLECT_LOGDIR}" && ls -tcp1 | grep '/$' | head -n 1)
+
+
+
+case "$1" in
+   config)
+cat << eof
+graph_title Backups speedup
+graph_category backup
+graph_vlabel speedup
+eof
+# create labels
+cd "${CSOURCES}"
+for source in *; do
+   name="_$(echo $source | sed 's/\./_/g')"
+   echo ${name}.label ${source}
+done
+
+
+
+      exit 0
+   ;;
+esac
+
