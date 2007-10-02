5039d93334669270780b7bbdb4f483bc9e67d98c
Nico Schottelius <nico@denkbrett.schottelius.org>
2007-10-02 16:28:38 +0200

    Add munin-script external/munin/ccollect_duration.sh

---
+++ external/munin/ccollect_duration.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Nico Schottelius
+# 2007-10-02
+# speedup
+
+CCOLLECT_CONF=${CCOLLECT_CONF:-/etc/ccollect}
+CSOURCES=${CCOLLECT_CONF}/sources
+CCOLLECT_LOGDIR=${CCOLLECT_LOGDIR:-/var/log/backup}
+latest_log=$(cd "${CCOLLECT_LOGDIR}" && ls -tcp1 | grep -v '/$' | head -n 1)
+LOGFILE="${CCOLLECT_LOGDIR}/${latest_log}"
+
+case "$1" in
+   config)
+cat << eof
+graph_title ccollect backup duration
+graph_category backup
+graph_vlabel time in seconds
+graph_info Shows the time needed by each source
+timeout 30
+eof
+# create labels
+cd "${CSOURCES}"
+for source in *; do
+   name="_$(echo $source | sed 's/\./_/g')"
+   echo ${name}.label ${source}
+   echo ${name}.min 0
+done
+      exit 0
+   ;;
+esac
+
+# get values
+cd "${CSOURCES}"
+for source in *; do
+   name="_$(echo $source | sed 's/\./_/g')"
+   value_raw="$(awk "/^\[${source}\] Backup lasted: / { print \$4 }" < "${LOGFILE}")"
+   value="$(echo $value | awk -F: '{ print $1 *3600 + $2 * 60 +  $3 }')"
+   # value = 0 = no result found
+   [ "$value" ] || value=0
+   echo ${name}.value "${value}"
+done
