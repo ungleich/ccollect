9c67edbf5b4e27b365cfed69ee0fd5a2d3cee69c
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-03-09 23:09:11 +0100

    add gpl3 header to ccollect_list_intervals.sh

--- tools/ccollect_list_intervals.sh
+++ tools/ccollect_list_intervals.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
+# 
+# 2006-2008 Nico Schottelius (nico-ccollect at schottelius.org)
+# 
+# This file is part of ccollect.
 #
-# Nico Schottelius <nico-ccollect //@// schottelius.org>
-# Date: 24-Jun-2006
-# Last Modified: -
-# Copying: GPLv3 (See file COPYING in top directory)
+# ccollect is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ccollect is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ccollect. If not, see <http://www.gnu.org/licenses/>.
+#
+# Initially written on 24-Jun-2006
 #
 
 CCOLLECT_CONF=${CCOLLECT_CONF:-/etc/ccollect}
