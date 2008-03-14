9a314d370e5af2b2cef93a8392463e1b309cd5c5
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-03-14 09:25:04 +0100

    update exec.sh

--- test/exec.sh
+++ test/exec.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 host="home.schottelius.org"
-
+host=""
+set -x
 pcmd()
 {
+   echo "$#", "$@"
    if [ "$host" ]; then
       ssh "$host" "$@"
    else
-      "$@"
+      $@
    fi
 }
 
-pcmd ls /
-pcmd cd /; ls "/is not there"
+#pcmd ls /
+#pcmd cd /; ls "/is not there"
+pcmd cd / && ls
